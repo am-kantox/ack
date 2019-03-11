bc7b11b8d44748b8d807f5d290a4330457b6447a
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2019-03-11 18:17:11 +0100

    CircleCI support.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ack.MixProject do
     [
       app: :ack,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
